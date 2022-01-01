1a21e0c2a6ec088a315c2727d56ebe62df874030
Nicholas Tapia <tapzx2@Nicholass-MacBook-Air.local>
2022-01-01 02:37:49 -0500

    finish mvp

--- categorize.py
+++ categorize.py
@@ -13,7 +13,27 @@ parser.add_argument('file', type=str, help="this is the file to categorize")
 #parser.add_argument('categories', type=str, help="this is the file that contains your categories")
 args = parser.parse_args()
 
-commands = ["exit", "undo", "new_filter"]
+def exit_program():
+    print('exiting program...')
+    sys.exit()
+
+def new_filter():
+    print("creating filter...")
+
+def undo():
+    print("undoing previous entry...")
+    if previously_written_row == -1:
+        print("sorry, can't undo. nothing has been entered yet")
+    else:
+        df.at[previously_written_row, 'category'] = "uncategorized"
+
+commands = {
+    "exit": exit_program,
+    "new_filter": new_filter,
+    "undo": undo
+}
+
+command_keys = [key for key in commands.keys()]
 
 categories = [
     "Advertising",
@@ -31,59 +51,113 @@ categories = [
     "Janitor and cleaning"
 ]
 
-filters = {"Aoufe":"Rent"}
+categories_str = ''
+
+for i in range(0, len(categories)):
+    categories_str += f'{i} : {categories[i]}\n'
+
+filters = {
+    "Aoufe":"Rent",
+    "SRP SUREPAY": "Utilities",
+    "Cleaner": "Janitor and cleaning"}
 
 header5 = ["date", "amount", "idk", "empty1", "details"]
 header6 = header5 + ["category"]
 
 def filter(df, filters):
-    print("running filter program")
+    print("running filter program...")
     for key, value in filters.items():
-        print(key, value)
         df.loc[df['details'].str.contains(key, case=False), "category"] = value
-    #df.loc[df['details'].str.contains("Aoufe", case=False), "category"] = "Rent"
-    print(df)
+    return df
 
+def prepare():
+    if not os.path.isdir('.backups'):
+        try:
+            os.mkdir('.backups')
+        except:
+            print('error making .backups directory')
+    if not os.path.isfile(args.file):
+        print(f"\n{args.file}" + " cannot be found\n".upper())
+        parser.print_help()
+        exit_program()
+    elif os.path.isfile(args.file):
+        df = pd.read_csv(args.file)
+        df_len = len(df.columns)
+        if df_len == 5:
+            df = pd.read_csv(args.file, names=header5)
+            df[header6[5]] = "uncategorized"
+            return df
+        elif df_len == 6:
+            df = pd.read_csv(args.file, names=header6)
+            return df
+    #os.rename(f"./{args.file}", f"./.backups/{args.file}_{today}")
 
-def write(df, start):
-    current_row_index = start
-    print(f"current row: {current_row_index}")
-    print(df.loc[[current_row_index]])
-    print("enter: value OR a command: " + ', '.join(commands))
-    user_input = input()
-    if user_input == 'exit':
-        sys.exit()
-    elif type(user_input) != int or user_input not in commands:
-        print('input isnt and interger or in the list of commands')
-        write(df, current_row_index)
+df = prepare()
 
-if not os.path.isdir('.backups'):
-    try:
-        os.mkdir('.backups')
-    except:
-        print('error making .backups directory')
-if not os.path.isfile(args.file):
-    print(f"\n{args.file}" + " cannot be found\n".upper())
-    parser.print_help()
-elif os.path.isfile(args.file):
-    df = pd.read_csv(args.file)
-    df_len = len(df.columns)
-    if df_len == 5:
-        df = pd.read_csv(args.file, names=header5)
-        df[header6[5]] = ""
-    elif df_len == 6:
-        df = pd.read_csv(args.file, names=header6)
-    #print(df)
+if 'uncategorized' in df['category'].values:
+    previously_written_row = -1
+    while 'uncategorized' in df['category'].values:
+        print("time to categorize!")
+        #start_index = df["category"].eq('uncategorized').idxmax()
+        #print(df.iloc[start_index:start_index+1])
+        # WORKING FROM HERE!!! LOOK LOOK HERE!!!
+        filter(df, filters)
+        start_index = df["category"].eq('uncategorized').idxmax()
+        print("\n===================\n")
+        print(df.iloc[start_index:start_index+1])
+        print(f"\nenter a command: {command_keys}\n")
+        print(f"or enter a data value:\n{categories_str}")
+        user_input = input()
+        for key, value in commands.items():
+            if user_input == key:
+                    value()
+                    continue
+        try:
+            if int(user_input) in range (0, len(categories)):
+                print(f"{user_input} selected")
+                df.at[start_index, 'category'] = categories[int(user_input)]
+                previously_written_row = start_index
+                print(f"write() previously written row: {previously_written_row}")
+            else:
+                print("that number isn't a category")
+        except:
+            print("I don't think you put a proper input in...")
 
-    # number of rows
-    print(df.shape[0])
-    print(df)
-    filter(df, filters)
+        else:
+            "user input not valid, please try again"
+        print("continuing...")
+        
+        #print(df)
+
+        # get user input
+        # if user_input = command
+            # run it
+        # elif user_input in range of values for categories
+            # update df row value
+            # write to temp file in case program crashes
+        # else
+            # let user know it was an invalid entry and we're trying again!
+        
+else:
+    print('no uncategorized values found')
+#
+# print(df["category"])
+#print(df["category"].eq('uncategorized'))
+#print(df["category"].eq('uncategorized').idxmax())
+#print(type(df["category"].ne('uncategorized').idxmax()))
+#while df["category"].ne('').idxmax() != False:
+#    print("while loop running")
+        # number of rows
+        #print(df.shape[0])
+        #df = filter(df, filters)
+        #start = df["category"].ne('').idxmax()
+        #write(df, start)
+        
     
 
     # first blank link of category column
-    #start = df["category"].ne('').idxmax()
-    #write(df, start)
+    
+    
 
     # filter()
     
